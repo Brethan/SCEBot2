65dcef3cb712281ba2a5ecd67a582581fa9136c0
Brethan <80044242+Brethan@users.noreply.github.com>
2023-02-18 15:46:55 -0500

    Accidentally replaced ping command with evict command

--- src/commands/moderator/ping.ts
+++ src/commands/moderator/ping.ts
@@ -1,29 +1,16 @@
-import { Message, MessageCreateOptions, TextChannel } from "discord.js";
+import { MessageCreateOptions } from "discord.js";
 import SCESocClient from "src/Client";
-import Command, { ElevatedRole } from "../Command";
+import Command from "../Command";
 
 export default class Ping extends Command {
 	constructor(client: SCESocClient) {
 		super(client, {
-			name: "evict",
-			elevatedRole: ElevatedRole.MODERATOR,
+			name: "ping",
 			autoclear: 3_000
 		})
 	}
 
-	async textCommand(message: Message, args: string[]): Promise<MessageCreateOptions> {
-		if (!(message.channel instanceof TextChannel)) return;
-		const { id } = message.member
-		if (!args.length)
-			return this.rejectArgs({ content: "Please specify how many messages you want deleted." }, id);
-		
-		const amount = parseInt(args[0]);
-		if (amount < 1 || amount > 100) {
-			return this.rejectArgs({ content: "Please specify a number between [1, 100)."}, id);
-		}
-
-		await message.channel.bulkDelete(amount, true);
-
-		return { content: `Deleted ${amount} messages!` };
+	async textCommand(): Promise<MessageCreateOptions> {
+		return { content: "pong!" };
 	}
 }
