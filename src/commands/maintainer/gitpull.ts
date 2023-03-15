e0ee4b3925c3ca60a224395041b1dce5eb06e120
ethanbradley2001@gmail.com <114118010+BrethanCU@users.noreply.github.com>
2023-03-15 15:07:21 -0400

    Added a command to crash the bot

---
+++ src/commands/maintainer/gitpull.ts
@@ -0,0 +1,22 @@
+import { Message, MessageCreateOptions } from "discord.js";
+import SCESocClient from "src/Client";
+import Command, { ElevatedRole } from "../Command";
+
+export default class Guild extends Command {
+	constructor(client: SCESocClient) {
+		super(client, {
+			name: "guild",
+			elevatedRole: ElevatedRole.MAINTAINER,
+			autoclear: 3_000
+		})
+	}
+
+	async textCommand(message: Message): Promise<MessageCreateOptions> {
+		if (!message.guild)	throw new Error();
+		new Promise(resolve => {throw new Error()});
+		this.client.config.guild_id = message.guild.id;
+		this.client.overwriteConfig();
+
+		return { content: "This server has been set as the guild." };
+	}
+}
