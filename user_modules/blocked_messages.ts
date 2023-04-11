ed344b8f3ff712bfe3c68c1040571694f8a6b862
Brethan <ethanbradley2001@gmail.com>
2023-04-11 03:15:38 -0400

    Maded the blocked messages thing a bit more rigid, may have to come back to this (pettiness war)

--- user_modules/blocked_messages.ts
+++ user_modules/blocked_messages.ts
@@ -1,10 +1,30 @@
 import { Message } from "discord.js"
+import { transliterate as tr } from "transliteration";
+
 const emojis = ["🇸", "🇹", "🇮", "🇳", "🇰", "🇾"];
+const target = "blocked messages";
+const isBlockedMessage = (content: string) => {
+	if (target.match(content))
+		return true;
+
+	const split = content.split(/[^\w]|_/g);
+	const firstLetters = split.map(str => str.charAt(0));
+	const lastLetters = split.map(str => str.charAt(str.length - 1));
+
+	const forwards = firstLetters.join("") + lastLetters.join("");
+	const backwards = firstLetters.reverse().join("") + lastLetters.reverse().join("")
+
+	if (target.match(forwards + backwards))
+		return true;
+
+	return false
+}
+
 export const blockedMessage = async (message: Message) => {
 	if (!message.member) return;
-
-	const content = message.content.toLowerCase();
-	if (!content.includes("blocked message")) return;
+	const content = tr(message.content.toLowerCase()).replace(/[^\w| ]|_/, " ")
+	
+	if (isBlockedMessage(content)) return;
 
 	for (const emoji of emojis) {
 		await message.react(emoji);
