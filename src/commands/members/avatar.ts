8e57c0e81450859b044d706e481177cacdfcc2f1
ethanbradley2001@gmail.com <114118010+BrethanCU@users.noreply.github.com>
2024-03-20 16:26:51 -0400

    Updated dependencies + finished avatar command

--- src/commands/members/avatar.ts
+++ src/commands/members/avatar.ts
@@ -1,4 +1,4 @@
-import { EmbedBuilder, Message, MessageCreateOptions } from "discord.js";
+import { EmbedBuilder, Message, MessageCreateOptions, MessageMentions } from "discord.js";
 import SCESocClient from "src/Client";
 import Command, { ElevatedRole } from "../Command";
 
@@ -11,17 +11,31 @@ export default class Avatar extends Command {
 	}
 
 	async textCommand(message: Message, args: string[]): Promise<MessageCreateOptions> {
-		const { member } = message;
+		const { member, guild } = message;
 
-		if (!member) throw new Error();
-		
+		if (!member || !guild) throw new Error();
+
 		let imageUrl: string = "";
-		if (!args.length) {
+		let name: string = "";
+
+		if (!args.length || args.length > 1) {
 			imageUrl = member.displayAvatarURL();
+			name = member.user.username;
+		} else if (args.length == 1) {
+			const target = message.mentions.members?.first();
+			if (!target) 
+				return { content: "Let's mention someone shall we?" };
+
+			imageUrl = target.displayAvatarURL();
+			name = target.user.username;
 		}
 		
 		const embed = new EmbedBuilder()
-			.setThumbnail(imageUrl);
+			.setDescription("**Server Avatar**")
+			.setColor("Yellow")
+			.setImage(imageUrl)
+			.setAuthor({ name, iconURL: imageUrl });
+
 
 		return { embeds: [embed] };
 
