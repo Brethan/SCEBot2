00e4389ad90aa0d8ce997c22b47345ef79a5f1bd
Brethan <ethanbradley2001@gmail.com>
2024-05-11 07:03:24 -0400

    Added 2 commands, editscrape and editpaste to copy and paste (or edit) message embeds

---
+++ src/commands/maintainer/embedpaste.ts
@@ -0,0 +1,43 @@
+import { APIEmbed, Message, MessageCreateOptions, TextChannel } from "discord.js";
+import { readFile, readdir } from "fs/promises";
+import SCESocClient from "src/Client";
+import Command, { ElevatedRole } from "../Command";
+
+export default class EmbedPaste extends Command {
+	constructor(client: SCESocClient) {
+		super(client, {
+			name: "embedpaste",
+			elevatedRole: ElevatedRole.MAINTAINER,
+		})
+	}
+
+	async textCommand(message: Message): Promise<MessageCreateOptions> {
+		// Filter through all of the copied embeds
+		const dir = "./data/embeds/";
+		const dirContent = (await readdir(dir))
+			.filter(file => file.endsWith(".json"))
+			.filter(file => !isNaN(parseInt(file.split(".")[0])))
+			.sort((a, b) => parseInt(a.split(".")[0]) - parseInt(b.split(".")[0]))
+		
+		if (!dirContent.length) {
+			return { content: "You do not currently have any saved embeds." };
+		}
+		
+		// Should be the most recent of the copied embeds
+		const filename = dir + dirContent.pop();
+		
+		const raw = await readFile(filename, { encoding: "utf8" });
+		const json: APIEmbed = JSON.parse(raw) satisfies APIEmbed;
+		json.thumbnail = { url: message.guild?.iconURL() || "" };
+
+		// if there's a channel mentioned in the message, send the embed to that channel
+		const channel = message.mentions.channels.first() as (TextChannel | undefined);
+		if (channel) {
+			channel.send({ embeds: [json] });
+			return { content: "Pasting most recently scraped embed in " + channel.toString() }
+		} else { // otherwise send it to the channel of invocation 
+			return { content: `Pasting most recently scraped embed: `, embeds: [json] };
+		}
+
+	}
+}
---
+++ src/commands/maintainer/embedscrape.ts
@@ -0,0 +1,52 @@
+import { Message, MessageCreateOptions, Snowflake, TextChannel } from "discord.js";
+import SCESocClient from "src/Client";
+import Command, { ElevatedRole } from "../Command";
+import { writeFile } from "fs/promises";
+
+export default class EmbedScrape extends Command {
+	constructor(client: SCESocClient) {
+		super(client, {
+			name: "embedscrape",
+			elevatedRole: ElevatedRole.MAINTAINER,
+			autoclear: 3_500
+		})
+	}
+
+	async textCommand(message: Message, args: string[]): Promise<MessageCreateOptions> {
+		const memberId = message.member?.id;
+
+		if (args.length != 2) {
+			return this.rejectArg(memberId);
+		}
+
+		// Message Id should be the last argument sent
+		const messageId = args.pop();
+		const channel = message.mentions.channels.first();
+
+		if (messageId?.match(/\<\#\d{10,}\>/) || !channel || !(channel instanceof TextChannel)) {
+			return this.rejectArg(memberId);
+		}
+
+		const messages = await channel.messages.fetch();
+		const target = messages.filter(m => m.id === messageId).first();
+
+		// If the message doesn't exist or there are no embeds attached
+		if (!target || !target.embeds.length) 
+			return this.rejectArg(memberId);
+		
+		// Write the embed data to the file system for later use.
+		const json = JSON.stringify(target.embeds.shift()?.toJSON(), null, 4);
+		const path = `./data/embeds/${(new Date()).getTime()}.json`
+
+		await writeFile(path, json)
+
+		return { content: `Maintainer, you can view the embed json in ${path}!` };
+	}
+
+	rejectArg(id: string | undefined): MessageCreateOptions {
+		const fail = "You should probably learn how to use this command: \n\n"
+				+ `${this.client.prefix + this.name} #text-channel-name <message-id>`
+
+		return super.rejectArgs({content: fail}, id || "", 18_000);
+	}
+}
